edd9146c1a7da9264a577a37bb02483622714f24
Kikozy <yuexuanxuanw@gmail.com>
2020-11-11 23:08:58 +0800

    正在实现登录x系统

--- nodejs/class01/app.js
+++
@@ -1,8 +0,0 @@
-let fs = require('fs')
-fs.stat('./app.js',(error,data)=>{
-    if(error){
-        console.log(error)
-        return
-    }
-    console.log(data.is())
-})
--- nodejs/express/app.js
+++ nodejs/express/app.js
@@ -1,11 +1,16 @@
 const express = require("express")
-const { send } = require("process")
-const bodyParser = require("body-parser")
+const bodyParser = require("body-Parser")
+const cors = require("cors")
 const cookieParser = require("cookie-Parser")
-const cors = require('cors');
-let app = express()
 
-app.use(cors());
+const app = express()
+//使用跨域
+app.use(cors())
+//使用cookie 传入一个加密的密钥？
+app.use(cookieParser("DiaoCan"))
+//使用post
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.all('*', (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -14,33 +19,36 @@ app.all('*', (req, res, next) => {
     res.header("Content-Type", "application/json;charset=utf-8");
     next();
   });
- // app.use(allowCrossDomain);//运用跨域的中间件
-
-app.get("/",(req,res)=>{
-    console.log(req.url)
-    res.send("giao")
+  app.post("/login",(req,res)=>{
+      console.log("有用户尝试登录")
+        if(req.body.data.user == "DiaoCan" && req.body.data.pass == "123"){
+            //通过 设置cookie
+            res.cookie("namae",req.body.data.user,{
+                maxAge: 1000*30,
+                signed: true,
+                path: "/"
+            })
+            return res.send({resultCode: 200, resultMsg: '登录成功'})
+        }else{
+            return res.send({resultCode: 400, resultMsg: '登陆失败'})
+        }
 })
+  app.get("/",(req,res)=>{
+    if(!req.signedCookies['namae']){
+        //没有内容
+        res.send("请登录")
+    }else{
+        res.send("欢迎你"+req.signedCookies["namae"])
+    }
+  })
+  app.get("/page2",(req,res)=>{
+    if(!req.signedCookies['namae']){
+        //没有内容
+        res.send("请登录")
+    }else{
+        res.send("欢迎你")
+    }
+  })
 
-app.get("/login/one",(req,res)=>{
-    console.log("one")
-    res.send("one")
-})
-//动态路由
-app.get("/login/:id",(req,res)=>{
-    res.send(`动态路由${req.params["id"]} 用户: ${req.query.user}密码: ${req.query.pass}`)
-    console.log("动态路由")
-})
-//[使用post中间件]接受form表单传递的数据
-app.use(bodyParser.urlencoded({extended: false}))
-//[使用post中间件]接受json数据
-app.use(bodyParser.json())
-//[使用cookie中间件]
-// app.use(cookieParser())
-
-app.post("/login",(req,res)=>{
-    console.log(req.body)
-    res.send(req.body)
-})
-
-app.listen(8848)
-console.log("server runing... port: 8848")
+app.listen(3000)
+console.log("runing port: 8848")
